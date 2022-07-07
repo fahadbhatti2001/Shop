a39924411314deaf6ef8740328e546447dfc08ff
Fahad Bhatti <fahadqayyumbhatti2001@gmail.com>
2022-07-07 23:30:43 +0500

    Change Navbar Color Orange

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class shop extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.orange
       ),
       home: HomePage(),
     );
