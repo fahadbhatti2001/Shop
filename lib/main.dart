02adb361025d2075fc28b695a8765f857e349db8
Fahad Bhatti <fahadqayyumbhatti2001@gmail.com>
2022-07-07 23:28:59 +0500

    Another Change

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class shop extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.orange
+        primarySwatch: Colors.purple
       ),
       home: HomePage(),
     );
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -24,7 +24,7 @@ class productsOverviewScreen extends StatelessWidget {
             loadedProducts[i].imageUrl,
           ),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 1,
+            crossAxisCount: 2,
             childAspectRatio: 2/3,
             crossAxisSpacing: 10,
             mainAxisSpacing: 10,
